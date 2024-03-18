a70b91fb7fb064f62f42f8b04f0432ba7a775dcf
Iryna Artymuk <artimyk@ukr.net>
2024-03-18 15:04:57 +0200

    finish home work 3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,74 @@
-const key = new Key();
+// interface KeyTemplate{
+//     signature: number
+//     getSignature():number
+// }
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Key {
+    private signature: string;
 
-house.openDoor(person.getKey());
+    constructor() {
+        // Генеруємо випадковий підпис для ключа
+        this.signature = Math.random().toString(36).substring(7);
+    }
 
-house.comeIn(person);
+    getSignature(): string {
+        return this.signature;
+    }
+}
 
 
-export {};
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+        this.door = false; // По замовчуванню двері закриті
+    }
+
+    abstract openDoor(key: Key): void;
+    abstract comeIn(person: Person): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true; // Відкриваємо двері
+            console.log('Двері відчинено.');
+        } else {
+            console.log('Невірний ключ. Двері залишаються закритими.');
+        }
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            console.log('Людина входить до будинку.');
+        } else {
+            console.log('Двері закриті. Людина не може ввійти.');
+        }
+    }
+}
+// Створюємо ключ, людину та будинок
+const myKey = new Key();
+const person = new Person(myKey);
+const myHouse = new MyHouse(myKey);
+
+// Відкриваємо двері та запрошуємо людину в будинок
+myHouse.openDoor(myKey);
+myHouse.comeIn(person);
